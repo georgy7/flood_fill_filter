5c8fe8fe10ca86eb1f55c5abe5ce1639d2313555
Georgy Ustinov <httpcatharsis@gmail.com>
2019-10-07 01:34:08 +0300

    Command-line arguments parser.

--- README.md
+++ README.md
@@ -11,32 +11,14 @@ the result pixel is white.
 For more information please read the python source code.
 It's pretty short and simple.
 
-
-## See the video demonstration on YouTube
-
-[![Flood Fill Filter video demonstration on YouTube](https://img.youtube.com/vi/1NPU90AELg0/0.jpg)](https://www.youtube.com/watch?v=1NPU90AELg0)
-
-
 ## Usage
 
-    lineara source.jpg | c/flood | lineara -b result.png
-
-## Contributing
-
-* Please do not send the code that isn't yours.
-  Or if you do not agree to publish it under the terms of MPL 2.0.
-* Please, add *your* copyright to the files you add/modify.
-
-Currently, the filter (even the C imlementation) is very very slow.
-I was wondering if one may be able to write a faster version.
-Or to optimize the existing C implementation.
-
-I will likely merge a pull request with a working implementation written
-in any suitable programming language (Go/C/D/Nim/Haskell/Lisp/Cobol?).
-It just must be in a separate folder like the prototype does.
+    flood_fill_filter input.jpg output.png
 
 ## Changelog
 
 2017-11-18 — Python prototype — 4m 51s
 
-2017-11-28 — C version — 1200 ms
+2017-11-28 — C version — 1.2s
+
+2018-10-07 — New Python implementation — 18s
--- flood_fill_filter/__main__.py
+++ flood_fill_filter/__main__.py
@@ -1,18 +1,66 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
 import sys
 
 import flood_fill_filter.flood as flood
 
+USAGE_EPILOG = '''Example:
+
+    flood_fill_filter input.jpg output.pcx
+    flood_fill_filter -r 5 input.jpg output.pcx
+    flood_fill_filter -d 0.05 input.jpg output.pcx
+
+It is recommended to leave the optional parameters at their default values.
+'''
+
+
+def float_from_0_to_1_exclusive(arg):
+    try:
+        f = float(arg)
+    except ValueError as e:
+        raise argparse.ArgumentTypeError(str(e))
+
+    if f <= 0.0:
+        raise argparse.ArgumentTypeError("Must be greater than zero.")
+    elif 1.0 <= f:
+        raise argparse.ArgumentTypeError("Must be less than one.")
+
+    return f
+
 
 def main():
-    input = flood.read_linear(sys.argv[1])
-    result = flood.filter(input)
+    parser = argparse.ArgumentParser(
+        description='Pixel-accurate JPEG-noise-resistant edge detection algorithm.',
+        epilog=USAGE_EPILOG,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    parser.add_argument('-d', '--diff', type=float_from_0_to_1_exclusive, default=0.1,
+                        help='Y (CIE XYZ) sensitivity. Default: 0.1.')
+
+    parser.add_argument('-a', '--activation-threshold', type=float_from_0_to_1_exclusive, default=0.45,
+                        help='The fraction of filled pixels within the fill window needed '
+                             'for the white pixel in the output. Default: 0.45.')
+
+    parser.add_argument('-r', '--radius', type=int, default=4, choices=range(1, 10),
+                        help='The fill window margin. The window width equals 2r+1. Default: 4.')
+
+    parser.add_argument('input')
+    parser.add_argument('output')
+
+    namespace = parser.parse_args(sys.argv[1:])
+
+    input = flood.read_linear(namespace.input)
+    result = flood.filter(input,
+                          y_threshold=namespace.diff,
+                          kernel_margin=namespace.radius,
+                          ratio_threshold=namespace.activation_threshold)
 
     flood.save(
         flood.to_8_bit(result * 255),
-        sys.argv[2]
+        namespace.output
     )
 
 
